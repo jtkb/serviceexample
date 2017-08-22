dde9502db1b31ec9896217f66e09d9a9fe778ddd
Kerry Billingham <contact@AvionicEngineers.com>
2017-08-22 03:52:13 +0100

    Karaf command that uses ConsumerService which has been bound to RegulatedUseService bound by Blueprint (Aries).

--- commands/src/main/java/com/example/commands/CommandTwo.java
+++ commands/src/main/java/com/example/commands/CommandTwo.java
@@ -1,20 +1,21 @@
-package com.example.consumertwo;
+package com.example.commands;
 
+import com.example.service.ConsumerService;
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
 
 @Command(scope = "command", name="two", description = "Initiates CommandTwo")
+@Service
 public class CommandTwo  implements Action{
 
-    private final ConsumerTwo consumerTwo;
-
-    public CommandTwo(final ConsumerTwo consumerTwo) {
-        this.consumerTwo = consumerTwo;
-    }
+    @Reference
+    private ConsumerService consumerService;
 
     @Override
     public Object execute() throws Exception {
-        consumerTwo.useRegulatedService();
+        consumerService.useRegulatedService();
         return null;
     }
 }
